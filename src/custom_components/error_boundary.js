d4a6b3bfb8e90d6e8129cf3c5b4361207c82cd81
TaiJon Smith <taijonsmith22@gmail.com>
2021-01-28 17:53:41 -0500

    added errory boundary to app

---
+++ src/custom_components/error_boundary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './css/error_boundary.css';
+
+export default class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error, errorInfo) {
+      // You can also log the error to an error reporting service
+      //logErrorToMyService(error, errorInfo);
+      console.log(error)
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        return (
+            <div id="error_message">
+                <h1>Something went wrong!</h1>
+                <h3>Please refresh the page and try again</h3>
+            </div>
+        )
+      }
+  
+      return this.props.children; 
+    }
+} 
