597c3a20034fdd1889a9defb6c6ee172821f5206
TaiJon Smith <taijonsmith22@gmail.com>
2021-02-26 08:35:18 -0500

    fixed error for last commit

--- src/custom_components/main_content/design_one.js
+++ src/custom_components/main_content/design_one.js
@@ -68,7 +68,6 @@ export default function DesignOne(props) {
 
     useEffect(() => {
         setTabIndex(0);
-        window.scrollTo({top: 0});
     }, [logged_in]);
 
     useEffect(() => {
--- src/custom_components/refresh_page.js
+++ src/custom_components/refresh_page.js
@@ -3,6 +3,10 @@ import './css/refresh_page.css';
 import { CircularProgress } from '@material-ui/core';
 
 export default function RefreshPage() {
+    React.useEffect(() => {
+        window.scrollTo({top: 0});
+    });
+
     return (
         <div className="center">
             <CircularProgress color="primary" />
